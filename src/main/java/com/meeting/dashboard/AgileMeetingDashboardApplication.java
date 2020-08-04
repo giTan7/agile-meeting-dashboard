56dab0cb21355c298ee0c72ad45c5e60e2eb3686
Tarannum Ara <tarannum.ara17@gmail.com>
2020-08-04 16:27:20 +0530

    Basic commit

---
+++ src/main/java/com/meeting/dashboard/AgileMeetingDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.meeting.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileMeetingDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileMeetingDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meeting/dashboard/AgileMeetingDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meeting.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileMeetingDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
